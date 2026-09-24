15e0cd306d4b18bdfad367d29be2ad53cd0f2c81
yamanoutitatsuya-lgtm <yamanouti.tatsuya@gmail.com>
2026-09-24 17:37:51 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* オフライン対応：アプリ本体は「ネット優先・失敗時は保存版」，それ以外は保存版優先 */
-const CACHE='tanto-v1';
-const CORE=['./','index.html','manifest.webmanifest','icons/icon-180.png','icons/icon-192.png','icons/icon-512.png','icons/maskable-512.png'];
+const CACHE='tanto-v2';
+const CORE=['./','index.html','manifest.webmanifest','icon-180.png','icon-192.png','icon-512.png','maskable-512.png'];
 self.addEventListener('install',e=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())); });
 self.addEventListener('activate',e=>{ e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())); });
 self.addEventListener('fetch',e=>{
